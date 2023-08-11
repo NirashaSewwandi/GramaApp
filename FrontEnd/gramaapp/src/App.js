8125bf0265bd6f397ffec1813d00d33934271e4a
Dharane-Segar <dharanesegar08@gmail.com>
2023-08-11 10:20:22 +0530

    Created folder structure

---
+++ FrontEnd/gramaapp/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          
+ 
+  
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
