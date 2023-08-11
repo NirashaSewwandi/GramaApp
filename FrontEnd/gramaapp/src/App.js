bae471c5f21e2e8cd9a2daf08b7e4df08b649258
Dharane-Segar <dharanesegar08@gmail.com>
2023-08-11 12:22:38 +0530

    Created user

--- FrontEnd/gramaapp/src/App.js
+++ FrontEnd/gramaapp/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Common/Login";
 import SignUp from "./Components/Common/SignUp";
+import Request from "./Components/User/Request";
 
 
 
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login/>} />
           <Route exact path="/signup" element={<SignUp/>}/>
+          <Route exact path="/request" element={<Request/>}/>
+
          
       
        
