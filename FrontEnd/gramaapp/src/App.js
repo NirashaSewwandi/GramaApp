fafedda4a7e13bcfaa86a18eef5c9e1b86f72a80
Zeta201 <dini2040gh@gmail.com>
2023-08-11 12:05:48 +0530

    added

--- FrontEnd/gramaapp/src/App.js
+++ FrontEnd/gramaapp/src/App.js
@@ -3,18 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Common/Login";
 import SignUp from "./Components/Common/SignUp";
 
-
-
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Login/>} />
-          <Route exact path="/signup" element={<SignUp/>}/>
-         
-      
-       
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
